9c141b3f00841d10100310963b0b6777d997db98
Kevin Pangottil <60909305+K5p5@users.noreply.github.com>
2022-05-04 22:29:46 -0500

    nferenc etime for wave2vec21

--- wave2vec2_pretrained.py
+++ wave2vec2_pretrained.py
@@ -1,6 +1,7 @@
 import librosa
 import torch
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
+import numpy as np
 
 tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
 model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
@@ -11,6 +12,23 @@ speech, rate = librosa.load('hero.wav',sr=16000)
 # print(rate)
 input_values = tokenizer(speech, return_tensors = 'pt').input_values
 print(input_values)
+# INIT LOGGERS
+starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+repetitions = 3
+timings=np.zeros((repetitions,1))
+#GPU-WARM-UP
+for _ in range(10):
+    _ = model(input_values)
+
+for rep in range(repetitions):
+  # Measure start time
+  starter.record()
+  inference = model(input_values)
+  ender.record()
+  # WAIT FOR GPU SYNC
+  torch.cuda.synchronize()
+  curr_time = starter.elapsed_time(ender)
+  timings[rep] = curr_time
 #Store logits (non-normalized predictions)
 predict = model(input_values) 
 print(predict)
@@ -23,3 +41,7 @@ print(predicted_ids)
 transcriptions = tokenizer.decode(predicted_ids[0])
 print("Predicted: ",transcriptions)
 print("Actual: You either die a hero, or you live long enough to see yourself become the villain")
+total_inference_time = 0
+for time in timings:
+  total_inference_time += time
+print(f"Average inference time: {total_inference_time/repetitions}")
